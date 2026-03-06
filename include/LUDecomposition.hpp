5ea08aa5311cab9548d28bd1ae0a07f4739eb146
aishwaryaK2024 <akhedkar522@gmail.com>
2026-03-06 23:33:56 +0530

    Updated LUDecomposition

--- include/LUDecomposition.hpp
+++ include/LUDecomposition.hpp
@@ -1,20 +1,63 @@
 #ifndef LU_H
 #define LU_H
-using namespace std;
+
 #include "LinearSystem.hpp"
+#include <cmath>
 
 class LUDecomposition : public LinearSystem {
-    vector<std::vector<double>> L, U;
-    vector<double> solution;
+protected:
+    std::vector<std::vector<double>> L, U;
 
 public:
     LUDecomposition(const Matrix& m);
+
+    virtual void solve() = 0;                      // pure virtual
+    virtual std::vector<double> getSolution() = 0; // pure virtual
+
+    virtual ~LUDecomposition() {}
+};
+
+
+class Doolittle : public LUDecomposition {
+    std::vector<double> solution;
+
+public:
+    Doolittle(const Matrix& m);
+
     void solve() override;
-    vector<double> getSolution() override;
+    std::vector<double> getSolution() override;
 
 private:
     void decompose();
 };
 
+
+class Crout : public LUDecomposition {
+    std::vector<double> solution;
+
+public:
+    Crout(const Matrix& m);
+
+    void solve() override;
+    std::vector<double> getSolution() override;
+
+private:
+    void decompose();
+};
+
+
+class Cholesky : public LUDecomposition {
+    std::vector<double> solution;
+
+public:
+    Cholesky(const Matrix& m);
+
+    void solve() override;
+    std::vector<double> getSolution() override;
+
+private:
+    void decompose();
+    bool isSymmetric();
+};
 
 #endif
