9ea1b85dc07f3e20a1969fa3e94c1ce2a7400f2e
Aishwarya Khedkar <160451524+aishwaryaK2024@users.noreply.github.com>
2026-04-01 22:47:24 +0530

    Added Iterative Solver

---
+++ include/IterativeSolver.hpp
@@ -0,0 +1,38 @@
+#ifndef ITERATIVESOLVER_H
+#define ITERATIVESOLVER_H
+
+#include "LinearSystem.hpp"
+#include <cmath>
+
+class IterativeSolver : public LinearSystem {
+protected:
+    int maxIter;
+    double tol;
+    std::vector<double> solution;
+
+public:
+    IterativeSolver(const Matrix& m, int maxIter = 1000, double tol = 1e-6);
+
+    virtual void solve() = 0;
+    virtual std::vector<double> getSolution() = 0;
+
+    virtual ~IterativeSolver() {}
+};
+
+
+class GaussJacobi : public IterativeSolver {
+public:
+    GaussJacobi(const Matrix& m, int maxIter = 1000, double tol = 1e-6);
+    void solve() override;
+    std::vector<double> getSolution() override;
+};
+
+
+class GaussSeidel : public IterativeSolver {
+public:
+    GaussSeidel(const Matrix& m, int maxIter = 1000, double tol = 1e-6);
+    void solve() override;
+    std::vector<double> getSolution() override;
+};
+
+#endif
